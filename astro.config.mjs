3f176ef1d3442b2df084895b6d4097cba2b5a3c7
Cristian Quintero <cristian181199@gmail.com>
2025-10-12 13:43:35 +0200

    feat: implement blog structure with MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cristianqg.dev',
-  integrations: [sitemap()],
+  integrations: [mdx(), sitemap()],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es', 'de'],
