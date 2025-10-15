37fce426bd6a771ef6cdbbfc25d34e5b143f643a
Cristian Quintero <cristian181199@gmail.com>
2025-10-15 20:43:42 +0200

    fix: Change output mode from hybrid to server for better compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cristianqg.dev',
-  output: 'hybrid',
+  output: 'server',
   adapter: node({
     mode: 'standalone'
   }),
